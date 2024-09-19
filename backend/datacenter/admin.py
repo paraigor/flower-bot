a70b4d48c6de0cb128b3fe267d18d7a833b06bdb
Misha <m.mazaixin@yandex.ru>
2024-09-19 10:11:51 +0300

    Squashed commit of the following:
    commit 46d6f00c8c034f161a68b44c1c819eeaebefc749
    Author: paraigor <103633539+paraigor@users.noreply.github.com>
    Date:   Wed Sep 18 20:22:37 2024 +0300
    
        Add models

--- backend/datacenter/admin.py
+++ backend/datacenter/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bunch, BunchElement, Client, EstimatedCost, Motive, Order
+
+
+class BunchElementInline(admin.TabularInline):
+    model = Bunch.elements.through
+    extra = 0
+
+
+@admin.register(Bunch)
+class BunchAdmin(admin.ModelAdmin):
+    inlines = [BunchElementInline]
+
+
+admin.site.register(BunchElement)
+admin.site.register(EstimatedCost)
+admin.site.register(Client)
+admin.site.register(Motive)
+admin.site.register(Order)
--- backend/datacenter/models.py
+++ backend/datacenter/models.py
@@ -1,3 +1,124 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Client(models.Model):
+    id_tg = models.IntegerField(
+        "id в телеграмм", blank=True, null=True, unique=True
+    )
+    full_name = models.CharField("ФИО", max_length=200)
+    phone_number = PhoneNumberField("Номер телефона", null=True, region="RU")
+
+    def __str__(self) -> str:
+        return self.full_name
+
+    class Meta:
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
+
+
+class Motive(models.Model):
+    title = models.CharField("Повод", max_length=200)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = "Повод"
+        verbose_name_plural = "Поводы"
+
+
+class EstimatedCost(models.Model):
+    title = models.CharField("Название", max_length=200)
+    value = models.FloatField("Рассчетная сумма")
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = "Рассчетная сумма"
+        verbose_name_plural = "Рассчетные суммы"
+
+
+class BunchElement(models.Model):
+    title = models.CharField("Название элемента букета", max_length=200)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = "Элемент букета"
+        verbose_name_plural = "Элементы букета"
+
+
+class Bunch(models.Model):
+    price = models.FloatField("Цена букета")
+    image = models.ImageField("Изображение букета", null=True, blank=True)
+    caption = models.TextField("Смысл букета", null=True, blank=True)
+    motive = models.ManyToManyField(
+        Motive,
+        related_name="bunch",
+        verbose_name="Повод",
+    )
+    elements = models.ManyToManyField(
+        BunchElement,
+        through="BunchAssembly",
+        related_name="bunch",
+        verbose_name="Элементы букета",
+    )
+
+    def __str__(self) -> str:
+        return f"Букет / {self.price} / {self.id}"
+
+    class Meta:
+        verbose_name = "Букет"
+        verbose_name_plural = "Букеты"
+
+
+class BunchAssembly(models.Model):
+    bunch = models.ForeignKey(
+        Bunch, on_delete=models.PROTECT, verbose_name="Букет"
+    )
+    element = models.ForeignKey(
+        BunchElement, on_delete=models.PROTECT, verbose_name="Элемент букета"
+    )
+    quantity = models.IntegerField("Количество", default=1)
+
+    def __str__(self) -> str:
+        return f"{self.bunch} / {self.element} / {self.quantity}"
+
+    class Meta:
+        verbose_name = "Элемент букета"
+        verbose_name_plural = "Элементы букета"
+
+
+class Order(models.Model):
+    STATUSES = [
+        ("accepted", "Принят"),
+        ("confirmed", "Подтвержден"),
+        ("completed", "Выполнен"),
+        ("canceled", "Отменен"),
+    ]
+    status = models.CharField(
+        "Статус заказа", max_length=9, choices=STATUSES, default="accepted"
+    )
+    date = models.DateField("Дата заказа", auto_now_add=True)
+    time = models.TimeField("Время заказа", auto_now_add=True)
+    client = models.ForeignKey(
+        Client, on_delete=models.PROTECT, verbose_name="Клиент"
+    )
+    bunch = models.ForeignKey(
+        Bunch, on_delete=models.PROTECT, verbose_name="Букет"
+    )
+    delivery_date = models.DateField("Дата доставки")
+    delivery_time = models.TimeField("Время доставки")
+    delivery_address = models.TextField(
+        verbose_name="Адрес доставки", max_length=200, null=True, blank=True
+    )
+
+    def __str__(self) -> str:
+        return f"{self.status} {self.date} {self.client.full_name}"
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
