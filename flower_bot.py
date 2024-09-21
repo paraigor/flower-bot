b23b0c679f5e51589696e8a00e3e738a3621099d
AndreyBychenkow <decebell032@yandex.ru>
2024-09-21 16:40:32 +0300

    Добавлен полный скрипт

--- flower_bot.py
+++ flower_bot.py
@@ -6,7 +6,10 @@ from telegram import InlineKeyboardMarkup, InlineKeyboardButton, Update
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, \
     MessageHandler, Filters, ConversationHandler
 
-EVENT, BUDGET, CUSTOM_EVENT = range(3)
+EVENT, BUDGET, CUSTOM_EVENT, NAME, ADDRESS, DATE, TIME, PHONE_NUMBER = range(8)
+
+COURIER_ID = '2082099178'
+FLORIST_ID = '3055049156'
 
 bouquets = [
     {
@@ -47,7 +50,7 @@ bouquets = [
     },
 ]
 
-
+# Функция приветствия
 def start_welcome(update: Update, context):
     keyboard = [
         [InlineKeyboardButton('День рождения', callback_data='День рождения'),
@@ -65,7 +68,7 @@ def start_welcome(update: Update, context):
     )
     return EVENT
 
-
+# Функция выбора повода
 def event_selection(update: Update, context):
     query = update.callback_query
     query.answer()
@@ -164,6 +167,61 @@ def budget_selection(update: Update, context):
 
         context.user_data['bold_text_sent'] = True
 
+    return ConversationHandler.END
+
+
+def order_bouquet(update: Update, context):
+    query = update.callback_query
+    query.answer()
+
+    context.bot.send_message(chat_id=query.message.chat_id,
+                             text="Отлично! Давайте оформим ваш заказ. Для начала, пожалуйста, введите ваше имя.")
+
+    return NAME
+
+
+def get_name(update: Update, context):
+    context.user_data['name'] = update.message.text
+    update.message.reply_text(
+        "Спасибо! Теперь, пожалуйста, введите адрес доставки.")
+    return ADDRESS
+
+
+def get_address(update: Update, context):
+    context.user_data['address'] = update.message.text
+    update.message.reply_text(
+        " Укажите дату доставки в формате ДД.ММ.ГГГГ.")
+    return DATE
+
+
+def get_date(update: Update, context):
+    context.user_data['date'] = update.message.text
+    update.message.reply_text(
+        " Укажите желаемое время доставки в формате ЧЧ:ММ.")
+    return TIME
+
+
+def get_time(update: Update, context):
+    context.user_data['time'] = update.message.text
+
+    # Сообщение для курьера
+    courier_message = f"Новый заказ!\n\n" \
+                      f"Имя: {context.user_data['name']}\n" \
+                      f"Адрес: {context.user_data['address']}\n" \
+                      f"Дата: {context.user_data['date']}\n" \
+                      f"Время: {context.user_data['time']}\n" \
+                      f"Событие: {context.user_data['event']}\n" \
+                      f"Бюджет: {context.user_data['budget']}"
+
+    # Отправка сообщение курьеру
+    context.bot.send_message(chat_id=COURIER_ID, text=courier_message)
+
+    # Подтверждение заказа пользователю
+    update.message.reply_text(
+        "Спасибо за ваш заказ! Мы передали информацию курьеру. Ожидайте доставку в указанное время.")
+
+    return ConversationHandler.END
+
 
 def cancel(update: Update, context):
     update.message.reply_text(
@@ -171,26 +229,126 @@ def cancel(update: Update, context):
     return ConversationHandler.END
 
 
+# Обработка запроса на консультацию
+def consultation_request(update: Update, context):
+    query = update.callback_query
+    query.answer()
+
+    query.message.reply_text(
+        "Укажите номер телефона, и наш флорист перезвонит вам в течение 20 минут.")
+    return PHONE_NUMBER
+
+
+def get_phone_number(update: Update, context):
+    phone_number = update.message.text
+    context.user_data['phone_number'] = phone_number
+
+    update.message.reply_text(
+        "Флорист скоро свяжется с вами. А пока можете присмотреть что-нибудь из готовой коллекции."
+    )
+
+    # Отправка нового букета и интерфейса
+    bouquet = random.choice(bouquets)
+    update.message.reply_text(
+        f"Вот подходящий букет для вас:\n\n"
+        f"{bouquet['description']}\n"
+        f"Состав: {bouquet['composition']}\n"
+        f"Стоимость: {bouquet['price']}"
+    )
+    context.bot.send_photo(chat_id=update.message.chat_id,
+                           photo=bouquet['image'])
+
+    keyboard = [
+        [
+            InlineKeyboardButton('Заказать букет', callback_data='order'),
+            InlineKeyboardButton('Посмотреть всю коллекцию',
+                                 callback_data='collection')
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text="Выберите вариант:",
+                             reply_markup=reply_markup)
+
+    return ConversationHandler.END
+
+
+def view_collection(update: Update, context):
+    query = update.callback_query
+    query.answer()
+
+    # Отправка нового букета из коллекции
+    bouquet = random.choice(bouquets)
+    query.message.reply_text(
+        f"Вот подходящий букет для вас:\n\n"
+        f"{bouquet['description']}\n"
+        f"Состав: {bouquet['composition']}\n"
+        f"Стоимость: {bouquet['price']}"
+    )
+    context.bot.send_photo(chat_id=query.message.chat_id,
+                           photo=bouquet['image'])
+
+    keyboard = [
+        [
+            InlineKeyboardButton('Заказать букет', callback_data='order'),
+            InlineKeyboardButton('Следующее фото', callback_data='collection')
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    context.bot.send_message(chat_id=query.message.chat_id,
+                             text="Выберите вариант:",
+                             reply_markup=reply_markup)
+
+
 def main():
     load_dotenv()
     telegram_bot_token = os.environ['YOUR_TG_TOKEN']
-    
+
     updater = Updater(telegram_bot_token, use_context=True)
     dp = updater.dispatcher
 
+    # Обработчики
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start_welcome)],
         states={
             EVENT: [CallbackQueryHandler(event_selection)],
             CUSTOM_EVENT: [MessageHandler(Filters.text & ~Filters.command,
                                           custom_event_input)],
-
             BUDGET: [CallbackQueryHandler(budget_selection)],
         },
         fallbacks=[CommandHandler('cancel', cancel)]
     )
 
+    order_handler = ConversationHandler(
+        entry_points=[CallbackQueryHandler(order_bouquet, pattern='^order$')],
+        states={
+            NAME: [MessageHandler(Filters.text & ~Filters.command, get_name)],
+            ADDRESS: [
+                MessageHandler(Filters.text & ~Filters.command, get_address)],
+            DATE: [MessageHandler(Filters.text & ~Filters.command, get_date)],
+            TIME: [MessageHandler(Filters.text & ~Filters.command, get_time)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    # Обработчик для консультаций
+    consultation_handler = ConversationHandler(
+        entry_points=[CallbackQueryHandler(consultation_request,
+                                           pattern='^consultation$')],
+        states={
+            PHONE_NUMBER: [MessageHandler(Filters.text & ~Filters.command,
+                                          get_phone_number)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
     dp.add_handler(conv_handler)
+    dp.add_handler(order_handler)
+    dp.add_handler(consultation_handler)
+    dp.add_handler(
+        CallbackQueryHandler(view_collection, pattern='^collection$'))
 
     updater.start_polling()
     updater.idle()
