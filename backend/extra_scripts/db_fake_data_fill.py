13413ee8eb7fd32ccd032816bfeb370336f0bdbe
Misha <m.mazaixin@yandex.ru>
2024-09-22 12:30:30 +0300

    Доработал скрипт заполнения БД

--- backend/extra_scripts/db_fake_data_fill.py
+++ backend/extra_scripts/db_fake_data_fill.py
@@ -2,12 +2,23 @@ from random import choice, randint
 
 from datacenter.models import (
     Bunch,
+    BunchAssembly,
     BunchElement,
     Client,
     Budget,
     Motive,
     Order,
 )
+
+
+STATUS = ["accepted", "confirmed"]
+ADDRESSES = [
+        "Москва, Тверская, 21, кв. 23",
+        "Москва, Краснопресненская, 46, кв. 355",
+        "Люберцы, Октябрьский проспект, 101, кв. 123",
+        "Химки, Калинина, 4А, кв. 145",
+        "Одинцово, Маршала Жукова, 46, кв. 299",
+    ]
 
 
 # Создание Клиентов
@@ -43,7 +54,7 @@ def create_occasions():
         "8 марта",
         "23 февраля",
         "14 февраля",
-        "Без повода",
+        "Другой повод",
     ]
 
     for occasion in occasions:
@@ -72,17 +83,18 @@ def create_elements_bouquet():
 # Создание Рссчетной суммы
 def create_budget():
     budgets = {
-        0: {"title": "До 1000", "price": 1000},
-        1: {"title": "До 2500 ", "price": 2500},
-        2: {"title": "До 5000", "price": 5000},
-        3: {"title": "Свыше 5000", "price": 10000},
-        4: {"title": "Не важно", "price": 1000000},
+        0: {"title": "до 1500", "value_from": 0, 'value_to': 1499},
+        1: {"title": "От 1500 до 3000 ", "value_from": 1500, 'value_to': 2999},
+        2: {"title": "От 3000 до 5000", "value_from": 3000, 'value_to': 4999},
+        3: {"title": "Свыше 5000", "value_from": 5000, 'value_to': 10000},
+        4: {"title": "Не важно", "value_from": 0, 'value_to': 10000},
     }
 
     for cost in budgets.values():
         title = cost["title"]
-        value = cost["price"]
-        Budget.objects.create(title=title, value=value)
+        value_from = cost["value_from"]
+        value_to = cost["value_to"]
+        Budget.objects.create(title=title, value_from=value_from, value_to=value_to)
 
 
 # Создание Букетов
@@ -90,39 +102,51 @@ def create_bouquets():
     bouquets = {
         0: {
             "title": "Sarah Bernardt",
-            "price": randint(250, 10000),
+            "price": randint(250, 1499),
             "image": "https://krasnodarflora.ru/upload/iblock/ce2/uisicyfvbd33mq75upcb3glng544bykc.jpg",
             "description": "Окутайте ее лавиной восторга! Этот великолепный букет - это больше, чем просто подарок - это захватывающее дух проявление любви, которое обещает оставить неизгладимое впечатление.",
         },
         1: {
             "title": "Букет из 25 гортензии",
-            "price": randint(250, 10000),
+            "price": randint(1500, 2999),
             "image": "https://krasnodarflora.ru/upload/iblock/c5b/z1s3yybxx8wukdjevpqdklxgibjgwyip.jpg",
             "description": "Добавьте нотку изысканности в ваш день с этим букетом!",
         },
         2: {
             "title": 'Букет "Гортензии и Альстромерии"',
-            "price": randint(250, 10000),
+            "price": randint(3000, 4999),
             "image": "https://krasnodarflora.ru/upload/iblock/6ed/9f6rgjx59afh2hhwszj7xffkb6ug5xo3.jpg",
             "description": "Букет в нежных тонах с гортензиями и альстромериями.",
         },
         3: {
             "title": 'Букет из 51 тюльпана "Солнце и Небо"',
-            "price": randint(250, 10000),
+            "price": randint(5000, 10000),
             "image": "https://krasnodarflora.ru/upload/iblock/680/6801e2c7d92a51490eb9c1a1f4b26e8f.jpg",
             "description": "Яркий и контрастный букет из 51 тюльпана",
         },
         4: {
             "title": "Букет из 10 лилий и 15 ирисов",
-            "price": randint(250, 10000),
+            "price": randint(250, 2999),
             "image": "https://krasnodarflora.ru/upload/iblock/85f/88drtlyxzdxvlterlrxk0u0e23n8ibid.jpg",
             "description": "Большой букет из 10 розовых лилий и 15 ирисов",
         },
         5: {
             "title": "Букет из белых роз, ирисов и ромашек",
-            "price": randint(250, 10000),
+            "price": randint(250, 2999),
             "image": "https://krasnodarflora.ru/upload/iblock/967/qbs0wbtizlbfenw4al6zmihum2yxvm5o.jpg",
             "description": "Яркий контрастный букет из 21 белой розы, 14 сиреневых ирисов и танацетума",
+        },
+        6: {
+            "title": 'Букет из 31 тюльпана "Солнце и Небо"',
+            "price": randint(3000, 10000),
+            "image": "https://krasnodarflora.ru/upload/iblock/680/6801e2c7d92a51490eb9c1a1f4b26e8f.jpg",
+            "description": "Яркий и контрастный букет из 31 тюльпана",
+        },
+        7: {
+            "title": "Букет из 15 гортензии",
+            "price": randint(3000, 10000),
+            "image": "https://krasnodarflora.ru/upload/iblock/c5b/z1s3yybxx8wukdjevpqdklxgibjgwyip.jpg",
+            "description": "Добавьте нотку изысканности в ваш день с этим букетом!",
         },
     }
 
@@ -142,28 +166,26 @@ def create_bouquets():
         )
 
         bunch.save()
+        
+        quantity = [num for num in range(1, 102, 2)]
+        for element in elements:
+            bunch_assembly = BunchAssembly(
+            bunch=bunch, element=element, quantity=choice(quantity)
+        )
+            bunch_assembly.save()
+            
         bunch.motive.set(motives)
         bunch.elements.set(elements)
 
 
 # Создание Заказов
 def create_orders():
-    status = ["Принят", "Подтвержден"]
+    clients = Client.objects.all()
+    bouquets = list(Bunch.objects.all())
 
-    addresses = [
-        "Москва, Тверская, 21, кв. 23",
-        "Москва, Краснопресненская, 46, кв. 355",
-        "Люберцы, Октябрьский проспект, 101, кв. 123",
-        "Химки, Калинина, 4А, кв. 145",
-        "Одинцово, Маршала Жукова, 46, кв. 299",
-    ]
-
-    clients = list(Client.objects.all())
-    bouquets = Bunch.objects.all()
-
-    for bouquet in bouquets:
-        status = choice(status)
-        client = choice(clients)
+    for client in clients:
+        status = choice(STATUS)
+        client = client
         bouquet = choice(bouquets)
         day = randint(1, 28)
         date = f"2024-10-{day:02d}"
@@ -172,7 +194,7 @@ def create_orders():
         time = f"{hour:02d}:{minute:02d}"
         delivery_date = f"2024-10-{day+1:02d}"
         delivery_time = f"{hour:02d}:00"
-        address = choice(addresses)
+        address = choice(ADDRESSES)
 
         Order.objects.create(
             status=status,
