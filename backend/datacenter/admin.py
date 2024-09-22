e180ab5657b7a2fd1240054b3d434c549b94c3f0
paraigor <103633539+paraigor@users.noreply.github.com>
2024-09-22 17:19:57 +0300

    Add send_order function

--- backend/datacenter/admin.py
+++ backend/datacenter/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 
 from .models import Budget, Bunch, BunchElement, Client, Motive, Order
 
+# def send_order():
+#     bot.send_message(
+#         chat_id=settings.COURIER_ID,
+#         text=f"""
+# Заказ №{order.id}:
+# Букет: {order.bunch.title}
+
+# Цена: {order.bunch.price}
+
+# Доставка:
+# {order.delivery_date} в {order.delivery_time}
+# Адрес: {order.delivery_address}
+# Комментарий: {order.comment}
+
+# Номер телефона:
+# {order.client.phone_number}""",
+#     )
 
 class BunchElementInline(admin.TabularInline):
     model = Bunch.elements.through
